ded58d5274cf7020d523958cbbc7b6cc3f1daf6c
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-11-10 16:41:06 +0000

    testing bot

--- grayskull/__main__.py
+++ grayskull/__main__.py
@@ -1,5 +1,20 @@
 import argparse
 import logging
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 import sys
 
@@ -73,13 +88,8 @@ def main(args=None):
         help="Disable or enable stdout, if it is False, Grayskull"
         " will disable the prints. Default is True",
     )
-    pypi_cmds.add_argument(
-        "--list-missing-deps",
-        default=False,
-        action="store_true",
-        dest="list_missing_deps",
-        help="After the execution Grayskull will print all the missing dependencies.",
-    )
+    pypi_cmds.add_argument(        "--list-missing-deps",        default=False,        action="store_true",        dest="list_missing_deps",        help="After the execution Grayskull will print all the missing dependencies.",
+)
     pypi_cmds.add_argument(
         "--strict-conda-forge",
         default=False,
